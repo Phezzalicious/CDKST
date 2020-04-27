e168c097f5366e100f37a1f313ead35d762f08f2
PhelpsMerrell <ptmerrell1@buffs.wtamu.edu>
2020-04-27 14:27:40 -0500

    myCommit

--- CDKST/Startup.cs
+++ CDKST/Startup.cs
@@ -120,7 +120,7 @@ namespace CDKST
             }
             else
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
